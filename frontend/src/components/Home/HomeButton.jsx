f81b5cc7c279c8c5c68107d1b4627adda390d263
Gopal Singh <gopalsin2910@gmail.com>
2025-06-07 17:10:20 +0530

    added home button properties

---
+++ frontend/src/components/Home/HomeButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/logo-final1.png';
+import './Homebutton.css';
+
+const HomeButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="dashboard">
+      <img src={logo} alt="Logo" className="Logo" onClick={() => navigate('/')} />
+      <div className="restdiv">
+        <text onClick={() => navigate('/profile')}>Profile</text>
+        <text onClick={() => navigate('/Dashboard')}>Dashboard</text>
+        <text onClick={() => navigate('/add-problem')}>Add Problem</text>
+        <text onClick={() => navigate('/problem-list')}>Problem List</text>
+        <text onClick={() => navigate('/Contest-Calendar')}>Contest-Calendar</text>
+      </div>
+      {/* <div className="logindiv">
+        <text onClick={() => navigate('/signup')}>Signup</text>
+        <text onClick={() => navigate('/login')}>Login</text>
+        <text onClick={() => navigate('/logout')}>Logout</text>
+      </div> */}
+    </div>
+  );
+};
+
+export default HomeButton;
